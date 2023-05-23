31bb66e15dba13158f417d8760d6661218bf9e17
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-05-23 05:36:50 +0200

    Play with color, phase 1

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,7 +16,7 @@ export const links: LinksFunction = () => [
 export const loader = async () => {
   // fetch page data from django wagtail backend here
   return json({
-    text: "my text",
+    text: "4慣ヨヱソ転理セリム座都ヲムニ横重ヤキ難家訪め平占由の季及投先ぼがゆも奇引りン員新ク大冤冶凋凌へぴイ。因クう首手代9意みの覧詞イいて走明コウキ視9皇辞ワ索毎フ悪部表ワス録浩ツ助頼柔ゅばル。由ちぐめ号好げず編風6横アタ残相げ橋稿モイカ床奉へで法区ク選公クで決30共るちス溶雄類仇佗ぽてこえ。 者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季21勤キシ名2破ンむ未聞写ス往回フクヒホ循50混ンろ上張おや志力久座員ど。1末うがいけ刊参組エヱヨ地生ユツコシ亮科く占辛おはて歓業こ売五びざゅ誕険かやあそ障子生ヌコ社数ルモ傳正天ぼ方茨さぱ成加ッせやか掲面にじがを原佐と気全ょねゆろ表元慈抹沙なッまお。my text",
   });
 };
 
@@ -25,11 +25,88 @@ export const loader = async () => {
 export default function Index() {
   const { text } = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1 className="heading">Welcome to the new Xlingual App</h1>
-      <p>{text}</p>
-      <button className="button">Button</button>
-      <a href="https://google.com">google</a>
-    </div>
+    <>
+      <div className="fullwidth-video">
+        <video className="video" playsInline autoPlay muted loop>
+          <source src="/video/dummy.mp4" type="video/mp4" />
+        </video>
+        <div className="overlay">
+          <div className="vinner">
+            <div className="vbanner">
+              <h2>Awesome lessons and learning experiences</h2>
+              <div className="vbanner__buttons">
+                <button className="button">Basic Button</button>
+                <button className="button button--red">Red Button</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="fullwidth">
+        <div className="text-container">
+          <h1 className="heading en">Welcome to the new Xlingual App</h1>
+          <p>{text}</p>
+          <button className="button">Basic Button</button>
+          <br></br>
+          <br></br>
+          <button className="button button--red">Red Button</button>
+        </div>
+      </div>
+      <div className="fullwidth green">
+        <div className="text-container">
+          <h2 className="en">This is green solid container</h2>
+          <h2>末うがいけ刊参組エヱヨ地生ユツコシ亮科</h2>
+          <p>{text}</p>
+        </div>
+      </div>
+      <div className="fullwidth">
+        <div className="text-container">
+          <h3>末うがいけ刊参組エヱヨ地生ユツコシ亮科</h3>
+          <ul>
+            <li>皇辞ワ索毎フ悪部表ノワス録浩ツ助頼柔ゅばル。</li>
+            <li>皇辞ワ索毎フ悪部表ノワス録浩ツ助頼柔ゅばル。</li>
+            <li>皇辞ワ索毎フ悪部表ノワス録浩ツ助頼柔ゅばル。</li>
+            <li>皇辞ワ索毎フ悪部表ノワス録浩ツ助頼柔ゅばル。</li>
+            <li>皇辞ワ索毎フ悪部表ノワス録浩ツ助頼柔ゅばル。</li>
+            <li>皇辞ワ索毎フ悪部表ノワス録浩ツ助頼柔ゅばル。</li>
+          </ul>
+        </div>
+      </div>
+      <div className="fullwidth dark">
+        <div className="text-container">
+          <h2 className="en">This is English</h2>
+          <h2>末うがいけ刊参組エヱヨ地生ユツコシ亮科</h2>
+          <p>{text}</p>
+        </div>
+      </div>
+      <div className="fullwidth cream-gradient">
+        <div className="text-container">
+          <h2 className="en">This is cream gradient</h2>
+          <h2>末うがいけ刊参組エヱヨ地生ユツコシ亮科</h2>
+          <p>{text}</p>
+        </div>
+      </div>
+      <div className="fullwidth green-gradient">
+        <div className="text-container">
+          <h2 className="en">This is a green gradient</h2>
+          <h2>末うがいけ刊参組エヱヨ地生ユツコシ亮科</h2>
+          <p>{text}</p>
+        </div>
+      </div>
+      <div className="fullwidth">
+        <div className="text-container">
+          <h2 className="en">This is English</h2>
+          <h2>末うがいけ刊参組エヱヨ地生ユツコシ亮科</h2>
+          <p>{text}</p>
+        </div>
+      </div>
+      <div className="fullwidth green">
+        <div className="text-container">
+          <h2 className="en">This is English</h2>
+          <h2>末うがいけ刊参組エヱヨ地生ユツコシ亮科</h2>
+          <p>{text}</p>
+        </div>
+      </div>
+    </>
   );
 }
