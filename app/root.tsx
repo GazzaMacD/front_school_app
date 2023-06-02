0761979ae8d41f1486bc450821577f28dcf5c34e
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-06-02 05:07:58 +0200

    Tablet ~ menu phase one completed

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,13 @@ import type { LinksFunction } from "@remix-run/node";
 import globalStyles from "./styles/global.css";
 import elementStyles from "./styles/element.css";
 import rootStyles from "./styles/root.css";
+import { AiFillPhone } from "react-icons/ai";
+import {
+  FaInstagram,
+  FaFacebookF,
+  FaLinkedinIn,
+  FaYoutube,
+} from "react-icons/fa";
 
 import {
   Links,
@@ -37,32 +44,111 @@ export default function App() {
             alt=""
           ></img>
           <div className="right-menu">
-            stuff
-            <div className="hamburger-system">
-              <input type="checkbox" id="navi-toggle" className="checkbox" />
-              <label htmlFor="navi-toggle" className="menu-button">
-                <span className="icon">&nbsp;</span>
+            <nav>
+              <ul className="right-menu__list">
+                <li className="right-menu__item">
+                  <a className="right-menu__link" href="tel:0561-42-5707">
+                    <AiFillPhone />
+                    <span>0561-42-5707</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <div className="mburger">
+              <input
+                type="checkbox"
+                id="navi-toggle"
+                className="mburger__checkbox"
+              />
+              <label htmlFor="navi-toggle" className="mburger__button">
+                <span className="mburger__icon">&nbsp;</span>
               </label>
-              <div className="background">&nbsp;</div>
-              <nav className="nav">
-                <ul className="list">
-                  <li className="item">
-                    <a className="link">Link 1 </a>
-                  </li>
-                  <li className="item">
-                    <a className="link">Link 1 </a>
-                  </li>
-                  <li className="item">
-                    <a className="link">Link 1 </a>
-                  </li>
-                  <li className="item">
-                    <a className="link">Link 1 </a>
-                  </li>
-                  <li className="item">
-                    <a className="link">Link 1 </a>
-                  </li>
-                </ul>
-              </nav>
+              <div className="mburger__background">&nbsp;</div>
+              <div className="mburger__menu">
+                <div className="mburger__left">
+                  <ul className="mburger__list">
+                    <li className="mburger__item">
+                      <a className="mb__link">Courses</a>
+                    </li>
+                    <li className="mburger__item">
+                      <a className="mb__link">Experiences</a>
+                    </li>
+                    <li className="mburger__item">
+                      <a className="mb__link">Prices</a>
+                    </li>
+                    <li className="mburger__item">
+                      <a className="mb__link">About us</a>
+                    </li>
+                    <li className="mburger__item">
+                      <a className="mb__link">Free Lessons</a>
+                    </li>
+                    <li className="mburger__item">
+                      <a className="mb__link">News</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="mburger__right">
+                  <h3 className="mburger__subheading">Other Links</h3>
+                  <div className="mburger__subblock">
+                    <nav>
+                      <ul className="mburger__list">
+                        <li className="mburger__item">
+                          <a className="mb__link mb__link--secondary">
+                            Link 1{" "}
+                          </a>
+                        </li>
+                        <li className="mburger__item">
+                          <a className="mb__link mb__link--secondary">
+                            Link 1{" "}
+                          </a>
+                        </li>
+                        <li className="mburger__item">
+                          <a className="mb__link mb__link--secondary">
+                            Link 1{" "}
+                          </a>
+                        </li>
+                        <li className="mburger__item">
+                          <a className="mb__link mb__link--secondary">
+                            Link 1{" "}
+                          </a>
+                        </li>
+                        <li className="mburger__item">
+                          <a className="mb__link mb__link--secondary">
+                            Link 1{" "}
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                  <h3 className="mburger__subheading">Social</h3>
+                  <div className="mburger__subblock">
+                    <nav>
+                      <ul className="mburger__social">
+                        <li>
+                          <a className="mburger__social-link">
+                            <FaInstagram size={"28px"} />
+                          </a>
+                        </li>
+                        <li>
+                          <a className="mburger__social-link">
+                            <FaFacebookF size={"26px"} />
+                          </a>
+                        </li>
+                        <li>
+                          <a className="mburger__social-link">
+                            <FaYoutube size={"32px"} />
+                          </a>
+                        </li>
+                        <li>
+                          <a className="mburger__social-link">
+                            <FaLinkedinIn size={"28px"} />
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </header>
