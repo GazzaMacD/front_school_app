6cb9fdc2af4e67112b934172f3ee55904a584929
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-06-18 07:11:07 +0200

    Login page error handling completed

---
+++ app/common/languageDictionary.ts
@@ -0,0 +1,14 @@
+export const MESSAGES = {
+  ja: {},
+  en: {
+    server: {
+      error: "Oops, sorry server issues",
+    },
+    network: {
+      error: "Oops, sorry there is a network problem",
+    },
+    form: {
+      standard400: "Form not submitted correctly, please try again.",
+    },
+  },
+};
--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -1,7 +1,65 @@
-import { Outlet, useSearchParams } from "@remix-run/react";
+import { useActionData, useSearchParams } from "@remix-run/react";
+import { type ActionArgs, json } from "@remix-run/node";
 import React from "react";
 
+import { login } from "~/common/session.server";
+import { MESSAGES } from "~/common/languageDictionary";
+import type { TLoginFail, TLoginActionResponse } from "~/common/types";
+
+/*
+ * Server Functions
+ */
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const email = form.get("email");
+  const password = form.get("password");
+  const redirectTo = form.get("redirect") || "/";
+  if (
+    typeof email !== "string" ||
+    typeof password !== "string" ||
+    typeof redirectTo !== "string"
+  ) {
+    return json<TLoginActionResponse>(
+      {
+        fields: null,
+        data: null,
+        errors: {
+          non_field_errors: [MESSAGES["en"].form.standard400],
+        },
+      },
+      { status: 400 }
+    );
+  }
+  const fields = { email, password };
+  const response = await login({ email, password });
+  if (!response.success) {
+    return json<TLoginActionResponse>(
+      {
+        fields: fields,
+        data: null,
+        errors: response.data as TLoginFail,
+      },
+      { status: 400 }
+    );
+  }
+  //success so create a session here and redirect
+  // or go to /dashboard
+  return json<TLoginActionResponse>(
+    {
+      fields: null,
+      data: null,
+      errors: null,
+    },
+    { status: 400 }
+  );
+}; //end action
+
+/*
+ * Client Functions
+ */
+
 export default function LoginRoute() {
+  const actionData = useActionData<typeof action>();
   const [searchParams] = useSearchParams();
   const [tempError, setTempError] = React.useState(false);
 
@@ -9,16 +67,40 @@ export default function LoginRoute() {
     <>
       <h1 className="auth__heading">Login</h1>
       <form noValidate method="post">
-        {tempError ? (
+        {actionData?.errors?.non_field_errors ? (
           <div className="form-nonfield-errors">
             <ul>
-              <li>Unknown error logging in</li>
+              {actionData.errors.non_field_errors.map((error) => (
+                <li role="alert" key={error}>
+                  {error}
+                </li>
+              ))}
             </ul>
           </div>
         ) : null}
         <div className="input-group">
           <label htmlFor="email-input">email</label>
-          <input type="email" id="email-input" name="email" />
+          <input
+            type="email"
+            id="email-input"
+            name="email"
+            defaultValue={actionData?.fields?.email}
+            aria-invalid={Boolean(actionData?.errors?.email?.length)}
+            aria-errormessage={
+              actionData?.errors?.email?.length ? "email-errors" : undefined
+            }
+          />
+          {actionData?.errors?.email?.length ? (
+            <ul
+              className="form-validation-errors"
+              role="alert"
+              id="email-errors"
+            >
+              {actionData.errors.email.map((error: string) => {
+                return <li key={error}>{error}</li>;
+              })}
+            </ul>
+          ) : null}
         </div>
         <div className="input-group">
           <label htmlFor="password-input">password</label>
@@ -26,11 +108,25 @@ export default function LoginRoute() {
             type="password"
             id="password-input"
             name="password"
-            aria-invalid="true"
+            defaultValue={actionData?.fields?.password}
+            aria-invalid={Boolean(actionData?.errors?.password?.length)}
+            aria-errormessage={
+              actionData?.errors?.password?.length
+                ? "password-errors"
+                : undefined
+            }
           />
-          <ul className="form-validation-errors">
-            <li>Password is to short</li>
-          </ul>
+          {actionData?.errors?.password?.length ? (
+            <ul
+              className="form-validation-errors"
+              role="alert"
+              id="password-errors"
+            >
+              {actionData.errors.password.map((error: string) => {
+                return <li key={error}>{error}</li>;
+              })}
+            </ul>
+          ) : null}
         </div>
         <button className="button submit " type="submit">
           sign in
