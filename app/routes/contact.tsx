b097806e60210bd7e5e7a843856c4013f8a00360
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-08-02 15:00:19 +0200

    Contact form implemented

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,12 +1,147 @@
 import type { LinksFunction } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
+import React from "react";
+import { type ActionArgs, type V2_MetaFunction, json } from "@remix-run/node";
 
-import contactStyles from "../styles/contact.css";
+import contactStyles from "~/styles/contact.css";
+import { BASE_API_URL } from "~/common/constants.server";
+import { MESSAGES } from "~/common/languageDictionary";
+
+/* Types */
+type TContactAPIResponse = {
+  details?: "ok";
+  email?: string[];
+  contact_notes?: {
+    note: string[];
+  }[];
+};
+type TContactErrors = {
+  non_field_errors?: string[];
+  fullName?: string[];
+  fullEnName?: string[];
+  email?: string[];
+  message?: string[];
+};
+type TContactActionResponse = {
+  fields: {
+    fullName: string;
+    fullEnName: string;
+    email: string;
+    message: string;
+  } | null;
+  data: { details: "ok" } | null;
+  errors: TContactErrors;
+};
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: contactStyles },
 ];
 
+function validateRequired(value: unknown): string[] {
+  if (!value) {
+    return [`This field is required`];
+  }
+  return [];
+}
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+
+  const fullName = form.get("fullName");
+  const fullEnName = form.get("fullEnName");
+  const email = form.get("email");
+  const message = form.get("message");
+  //basic type validations - most validation done on backend
+  if (
+    typeof fullName !== "string" ||
+    typeof fullEnName !== "string" ||
+    typeof email !== "string" ||
+    typeof message !== "string"
+  ) {
+    return json<TContactActionResponse>(
+      {
+        fields: null,
+        data: null,
+        errors: {
+          non_field_errors: [MESSAGES["en"].form.standard400],
+        },
+      },
+      { status: 400 }
+    );
+  }
+
+  const errors: TContactErrors = {
+    non_field_errors: [],
+    fullName: validateRequired(fullName),
+    fullEnName: validateRequired(fullEnName),
+    email: validateRequired(email),
+    message: validateRequired(message),
+  };
+  const fields = { fullName, fullEnName, email, message };
+  if (!Object.values(errors).every((v) => !v.length)) {
+    return json<TContactActionResponse>(
+      {
+        fields,
+        data: null,
+        errors,
+      },
+      { status: 400 }
+    );
+  }
+  /* back end */
+  try {
+    const response = await fetch(`${BASE_API_URL}/contact/form/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: fullName,
+        name_en: fullEnName,
+        contact_emails: [{ email: email }],
+        contact_notes: [{ note: message }],
+      }),
+    });
+    const responseData: TContactAPIResponse = await response.json();
+    if (!response.ok) {
+      errors.email = responseData.email ? responseData.email : [];
+      errors.message = responseData.contact_notes
+        ? responseData.contact_notes[0].note
+        : [];
+      return json<TContactActionResponse>(
+        {
+          fields: fields,
+          data: null,
+          errors,
+        },
+        { status: 400 }
+      );
+    }
+    return json<TContactActionResponse>(
+      {
+        fields: null,
+        data: { details: "ok" },
+        errors,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    // network error
+    errors.non_field_errors = [MESSAGES["en"].form.standard400];
+    return json(
+      {
+        fields,
+        data: null,
+        errors,
+      },
+      { status: 400 }
+    );
+  } // catch
+}; // action
+
 export default function Contact() {
+  const actionData = useActionData<typeof action>();
+  const maxMsgLength = 200;
   return (
     <div>
       <header className="container">
@@ -22,9 +157,127 @@ export default function Contact() {
         <h2>Common questions</h2>
       </section>
       <section className="container">
-        <div className="contact-wrapper">
-          <h2>Contact us</h2>
-          <form action="">Form here</form>
+        <div className="cf-wrapper">
+          <h2 className="cf-heading">Contact us</h2>
+          <form className="cf" noValidate method="POST">
+            <div className="cf__half">
+              <div className="input-group">
+                <label className="required" htmlFor="full-name-input">
+                  full name
+                </label>
+                <input
+                  type="text"
+                  id="full-name-input"
+                  name="fullName"
+                  defaultValue={actionData?.fields?.fullName}
+                  aria-invalid={Boolean(actionData?.errors?.fullName?.length)}
+                  aria-errormessage={
+                    actionData?.errors?.fullName?.length
+                      ? "full-name-errors"
+                      : undefined
+                  }
+                />
+                {actionData?.errors?.fullName?.length ? (
+                  <ul
+                    className="form-validation-errors"
+                    role="alert"
+                    id="full-name-errors"
+                  >
+                    {actionData.errors.fullName.map((error: string) => {
+                      return <li key={error}>{error}</li>;
+                    })}
+                  </ul>
+                ) : null}
+              </div>
+              <div className="input-group">
+                <label className="required" htmlFor="full-en-name-input">
+                  full english name
+                </label>
+                <input
+                  type="text"
+                  id="full-en-name-input"
+                  name="fullEnName"
+                  defaultValue={actionData?.fields?.fullEnName}
+                  aria-invalid={Boolean(actionData?.errors?.fullEnName?.length)}
+                  aria-errormessage={
+                    actionData?.errors?.fullEnName?.length
+                      ? "full-en-name-errors"
+                      : undefined
+                  }
+                />
+                {actionData?.errors?.fullEnName?.length ? (
+                  <ul
+                    className="form-validation-errors"
+                    role="alert"
+                    id="full-en-name-errors"
+                  >
+                    {actionData.errors.fullEnName.map((error: string) => {
+                      return <li key={error}>{error}</li>;
+                    })}
+                  </ul>
+                ) : null}
+              </div>
+              <div className="input-group">
+                <label className="required" htmlFor="email-input">
+                  email
+                </label>
+                <input
+                  type="email"
+                  id="email-input"
+                  name="email"
+                  defaultValue={actionData?.fields?.email}
+                  aria-invalid={Boolean(actionData?.errors?.email?.length)}
+                  aria-errormessage={
+                    actionData?.errors?.email?.length
+                      ? "email-errors"
+                      : undefined
+                  }
+                />
+                {actionData?.errors?.email?.length ? (
+                  <ul
+                    className="form-validation-errors"
+                    role="alert"
+                    id="email-errors"
+                  >
+                    {actionData.errors.email.map((error: string) => {
+                      return <li key={error}>{error}</li>;
+                    })}
+                  </ul>
+                ) : null}
+              </div>
+            </div>
+            <div className="input-group">
+              <label className="required" htmlFor="message-input">
+                message
+              </label>
+              <textarea
+                id="message-input"
+                maxLength={maxMsgLength}
+                name="message"
+                defaultValue={actionData?.fields?.message}
+                aria-invalid={Boolean(actionData?.errors?.message?.length)}
+                aria-errormessage={
+                  actionData?.errors?.message?.length
+                    ? "message-errors"
+                    : undefined
+                }
+              ></textarea>
+              {actionData?.errors?.message?.length ? (
+                <ul
+                  className="form-validation-errors"
+                  role="alert"
+                  id="message-errors"
+                >
+                  {actionData.errors.message.map((error: string) => {
+                    return <li key={error}>{error}</li>;
+                  })}
+                </ul>
+              ) : null}
+            </div>
+            <button className="button submit " type="submit">
+              Send
+            </button>
+          </form>
         </div>
       </section>
     </div>
