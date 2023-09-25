72d85bdcd74fdaec737d2da02a38e32e88e4a607
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-09-25 10:55:39 +0200

    About page html and basics completed

--- app/common/utils.ts
+++ app/common/utils.ts
@@ -4,8 +4,10 @@ import { createGlobalEnvObj, type TGlobalEnv } from "~/env.server";
  */
 export function getGlobalEnv(): TGlobalEnv {
   if (typeof window !== "undefined") {
+    // on client side
     return window.GLOBAL_ENV;
   } else {
+    // on server side
     return createGlobalEnvObj();
   }
 }
