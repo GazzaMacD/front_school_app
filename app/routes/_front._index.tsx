1e1cc4f1d2dcc33fdb74c04b88577aabb777f721
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-10-19 15:56:15 +0200

    Very basic home page testimonial section

--- app/routes/_front._index.tsx
+++ app/routes/_front._index.tsx
@@ -1,8 +1,10 @@
 import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import homeStyles from "../styles/home.css";
 import { getTitle } from "~/common/utils";
+import { BASE_API_URL } from "~/common/constants.server";
+import { getGlobalEnv } from "~/common/utils";
 
 // server side functions
 export const meta: V2_MetaFunction = () => {
@@ -14,16 +16,23 @@ export const links: LinksFunction = () => [
 ];
 
 export const loader = async () => {
-  // fetch page data from django wagtail backend here
-  return json({
-    text: "転理セリム座都ヲムニ横重ヤキ難家訪め平占由の季及投先ぼがゆも奇引りン員新ク大冤冶凋凌へぴイ。因クう首手代9意みの覧詞イいて走明コウキ視9皇辞ワ索毎フ悪部表ワス録浩ツ助頼柔ゅばル。由ちぐめ号好げず編風6横アタ残相げ橋稿モイカ床奉へで法区ク選公クで決30共るちス溶雄類仇佗ぽてこえ。 者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季21勤キシ名2破ンむ未",
-  });
+  try {
+    const testimonialsUrl = `${BASE_API_URL}/pages/?type=testimonials.TestimonialDetailPage&fields=slug,customer_name,customer_image,occupation,organization_name,comment&limit=10`;
+    const testimonialRes = await fetch(testimonialsUrl);
+    const testimonialData = await testimonialRes.json();
+    return json({
+      testimonials: testimonialData.items,
+    });
+  } catch (error) {
+    throw new Response("sorry, that is a 500", { status: 500 });
+  }
 };
 
 // --------------------------------//
 // client side functions
 export default function Index() {
-  const { text } = useLoaderData<typeof loader>();
+  const { testimonials } = useLoaderData<typeof loader>();
+  const ENV = getGlobalEnv();
   return (
     <>
       <section id="video-banner">
@@ -61,6 +70,65 @@ export default function Index() {
         </div>
       </section>
 
+      <section id="testimonials" className="full-width-container">
+        <hgroup className="bsection-group">
+          <h2 className="bsection-group__heading">Customer Testimonials</h2>
+          <p className="bsection-group__subheading">
+            占由の季及投先ぼがゆも奇引りン
+          </p>
+        </hgroup>
+        <div>
+          <h3>On Google</h3>
+        </div>
+        <div>
+          <h3>On our site</h3>
+          <div className="test-students">
+            {testimonials.length &&
+              testimonials.map((testimonial) => {
+                return (
+                  <article key={testimonial.id} className="test-testimonial">
+                    <header className="test-testimonial__header">
+                      <div className="test-testimonial__headimage">
+                        <img
+                          className="test-testimonial__image"
+                          src={`${ENV.BASE_BACK_URL}${testimonial.customer_image.thumbnail.src}`}
+                          alt={testimonial.customer_image.thumbnail.alt}
+                        />
+                      </div>
+                      <div className="test-testimonial__headtext">
+                        <h4 className="test-testimonial__name">
+                          {testimonial.customer_name}
+                        </h4>
+                        {testimonial.occupation && (
+                          <p className="test-testimonial__occu">
+                            {testimonial.occupation}
+                          </p>
+                        )}
+                        {testimonial.organization_name && (
+                          <p className="test-testimonial__org">
+                            {testimonial.organization_name}
+                          </p>
+                        )}
+                      </div>
+                    </header>
+                    <p className="test-testimonial__comment">
+                      {testimonial.comment.length > 40
+                        ? `${testimonial.comment.slice(0, 40)}...`
+                        : testimonial.comment}
+                      <Link
+                        className="test-testimonial__link"
+                        to={`/testimonials/${testimonial.meta.slug}`}
+                      >
+                        Read more and watch interview
+                      </Link>
+                    </p>
+                  </article>
+                );
+              })}
+          </div>
+        </div>
+      </section>
+
       <section id="popular">
         <hgroup className="bsection-group">
           <h2 className="bsection-group__heading">Most popular courses</h2>
@@ -86,80 +154,6 @@ export default function Index() {
             占由の季及投先ぼがゆも奇引りン
           </p>
         </hgroup>
-      </section>
-
-      <section id="free">
-        <hgroup className="bsection-group">
-          <h2 className="bsection-group__heading">Learn for free</h2>
-          <p className="bsection-group__subheading">
-            占由の季及投先ぼがゆも奇引りン
-          </p>
-        </hgroup>
-      </section>
-
-      <section className="bgrid">
-        <div className="btext">
-          <h1>誕険かやあそ障子生ヌコ社数ルモ</h1>
-          <p>{text}</p>
-          <h2>誕険かやあそ障子生ヌコ社数ルモ</h2>
-          <p>{text}</p>
-          <h3>誕険かやあそ障子生ヌコ社数ルモ</h3>
-          <p>
-            転理セリム座都
-            <a href="https://google.com">ヲムニ横重ヤキ難家訪め平</a>
-            占由の季及投先ぼがゆも奇引りン員新ク大冤冶凋凌へぴイ。因クう首手代9意みの覧詞イいて走明コウキ視9皇辞ワ索毎フ悪部表ワス録浩ツ助頼柔ゅばル。由ちぐめ号好げず編風6横アタ残相げ橋稿モイカ床奉へで法区ク選公クで決30共るちス溶雄類仇佗ぽてこえ。
-            者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季21勤キシ名2破ンむ未
-          </p>
-          <h3>リッスト</h3>
-          <ul>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-          </ul>
-          <h3>リッスト</h3>
-          <ol>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-            <li>者ヘキヨケ田治フ聖鋭セレ生声意断ヌヤワユ季</li>
-          </ol>
-          <h4>誕険かやあそ障子生ヌコ社数ルモ</h4>
-          <p>{text}</p>
-          <button className="button">Basic Button</button>
-        </div>
-        <figure className="full-image">
-          <img src="/img/class.jpg" alt="Students" />
-          <figcaption>Students</figcaption>
-        </figure>
-        <div className="btext">
-          <h2 className="en">Section</h2>
-          <p>{text}</p>
-        </div>
-        <div className="btext">
-          <h2 className="en">Section</h2>
-          <p>{text}</p>
-        </div>
-        <figure className="text-width-image">
-          <img src="/img/class.jpg" alt="Students" />
-          <figcaption>Students</figcaption>
-        </figure>
-        <div className="btext">
-          <h2 className="en">Section</h2>
-          <p>{text}</p>
-        </div>
-        <figure className="three-quarter-image">
-          <img src="/img/class.jpg" alt="Students" />
-          <figcaption>Students</figcaption>
-        </figure>
-        <div className="btext">
-          <h2 className="en">Section</h2>
-          <p>{text}</p>
-        </div>
       </section>
     </>
   );
