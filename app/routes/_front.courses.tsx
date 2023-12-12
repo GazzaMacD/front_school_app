04d17c4303d33227efa68e35ae1ffdb7fca97cef
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-12-12 15:42:48 +0100

    Add course parent page and styles sheet

---
+++ app/routes/_front.courses.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "@remix-run/react";
+import { type LinksFunction } from "@remix-run/node";
+
+import courseStyles from "../styles/courses.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: courseStyles },
+];
+
+/*
+ * client side code
+ */
+export default function CoursesParentPage() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
