207c120c3d9add7f17f2cd1922f15f50fb5ac3c1
Gareth Macdonald <macdonald.gareth@gmail.com>
2023-12-15 05:23:21 +0100

    Stage one of course index page complete, needs styling and separation of courses into topic

--- app/routes/_front.courses._index.tsx
+++ app/routes/_front.courses._index.tsx
@@ -2,11 +2,12 @@ import { Link, useLoaderData } from "@remix-run/react";
 import { type LinksFunction, json } from "@remix-run/node";
 
 import { BASE_API_URL } from "~/common/constants.server";
-/*
+import { getGlobalEnv } from "~/common/utils";
+
 export async function loader() {
   try {
-    const lPageUrl = `${BASE_API_URL}/pages/?type=languageschools.LanguageSchoolListPage&fields=*`;
-    const dPageUrl = `${BASE_API_URL}/pages/?type=languageschools.LanguageSchoolDetailPage&fields=title,display_title,ls`;
+    const lPageUrl = `${BASE_API_URL}/pages/?type=courses.CourseDisplayListPage&fields=*`;
+    const dPageUrl = `${BASE_API_URL}/pages/?type=courses.CourseDisplayDetailPage&fields=_,title,display_title,slug,id,subject_slug`;
     const urls = [lPageUrl, dPageUrl];
     const [lPage, dPage] = await Promise.all(
       urls.map((url) =>
@@ -24,7 +25,7 @@ export async function loader() {
     if (lPage.status !== 200 || dPage.status !== 200) {
       throw new Error(JSON.stringify({ lPage, dPage }, null, 2));
     }
-    if (!lPage.data.items.length || !dPage.data.items.length) {
+    if (!lPage.data.items.length) {
       throw new Response("Oops, that is not found", { status: 404 });
     }
     return json({
@@ -36,14 +37,77 @@ export async function loader() {
     throw new Response("oops that is an error", { status: 500 });
   }
 }
-*/
 
 export default function CoursesIndexPage() {
-  //const { listPage: lp, dPages: dp } = useLoaderData<typeof loader>();
+  const { listPage: lp, dPages: dp } = useLoaderData<typeof loader>();
+  const ENV = getGlobalEnv();
 
   return (
-    <div className="container">
-      <h1>Courses List Page</h1>
-    </div>
+    <>
+      <header className="container">
+        <hgroup className="heading1">
+          <h1>
+            <span>{lp.title}</span>
+            {lp.display_title}
+          </h1>
+          <p>{lp.display_tagline}</p>
+        </hgroup>
+      </header>
+      <section>
+        <hgroup>
+          <h2>
+            <span>{lp.en_sec_title}</span>
+            <br />
+            {lp.en_sec_dis_title}
+          </h2>
+        </hgroup>
+        <section>
+          <h3>{lp.en_sec_pop_title}</h3>
+          <div>
+            {lp.pop_en_courses.map((cs) => {
+              return (
+                <Link
+                  key={cs.id}
+                  to={`/courses/${cs.course.subject_slug}/${cs.course.slug}`}
+                >
+                  <article>
+                    <img
+                      src={`${ENV.BASE_BACK_URL}${cs.course.image.thumbnail.src}`}
+                      alt={cs.course.image.thumbnail.alt}
+                    />
+                    <section>
+                      <hgroup>
+                        <h4>
+                          {cs.course.title} <br /> {cs.course.display_title}
+                        </h4>
+                      </hgroup>
+                    </section>
+                  </article>
+                </Link>
+              );
+            })}
+          </div>
+        </section>
+        <section>
+          <h3>{lp.en_sec_other_title}</h3>
+          <div>
+            {dp.map((cs) => {
+              return (
+                <Link
+                  key={cs.id}
+                  to={`/courses/${cs.subject_slug}/${cs.meta.slug}`}
+                >
+                  <div>
+                    {cs.title}
+                    <br />
+                    {cs.display_title}
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </section>
+      </section>
+    </>
   );
 }
