79e506e7172fa07d39b27142c2dfc6af2ba132e7
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-01-11 15:34:46 +0100

    Services stage 1 completed

--- app/components/headings.tsx
+++ app/components/headings.tsx
@@ -2,14 +2,20 @@ type THeadingOne = {
   jpText: string;
   enText: string;
   align: "left" | "center";
+  bkground: "light" | "dark";
 };
 
-function HeadingOne({ enText, jpText, align }: THeadingOne) {
+function HeadingOne({
+  enText,
+  jpText,
+  align,
+  bkground = "light",
+}: THeadingOne) {
   return (
     <>
-      <h2 className={`g-heading1 ${align}`} lang="en">
+      <h2 className={`g-heading1 ${align} ${bkground}`} lang="en">
         {enText}
-        <span lang="ja" className={`g-heading1__jp ${align}`}>
+        <span lang="ja" className={`g-heading1__jp`}>
           ― {jpText}
         </span>
       </h2>
