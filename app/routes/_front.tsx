581df83976b10d9688a3d64074a7571b521744e9
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-01-18 10:02:50 +0100

    Stage one of footer dev, large screens only

--- app/routes/_front.tsx
+++ app/routes/_front.tsx
@@ -14,8 +14,8 @@ export default function FrontParentRoute() {
         <Outlet />
       </div>
       <footer className="footer">
-        <div className="container footer__top">
-          <div className="footer__branding-col">
+        <div className="container footer__inner">
+          <div className="footer__branding-col one">
             <Link to="/">
               <h2>XLingual</h2>
             </Link>
@@ -31,32 +31,73 @@ export default function FrontParentRoute() {
                 <FaYoutube className="footer__social-icon" />
               </Link>
             </div>
+            <div className="footer__copyright">
+              <p>
+                © Copyright 2023 株式会社XLingual | All Rights Reserved |
+                Website by digital4.green
+              </p>
+            </div>
           </div>
           <div className="footer__col">
-            <h3>Learning</h3>
-            <Link to="/courses">Our courses</Link>
-            <Link to="/learning-experiences">Our experiences</Link>
-            <Link to="/lessons">Free lessons</Link>
-            <h3>Company</h3>
-            <Link to="/about">About Us</Link>
-            <Link to="/schools">Our Schools</Link>
-            <Link to="/news">News</Link>
+            <h3>言語学習</h3>
+            <ul>
+              <li>
+                ― <Link to="/courses">コース紹介</Link>
+              </li>
+              <li>
+                ― <Link to="/prices">料金</Link>
+              </li>
+              <li>
+                ―{" "}
+                <Link to="/learning-experiences">
+                  ランゲージ・エクスペリエンス
+                </Link>
+              </li>
+              <li>
+                ― <Link to="/courses">読んで学べるブログ</Link>
+              </li>
+            </ul>
+
+            <h3>会社案内</h3>
+            <ul>
+              <li>
+                ― <Link to="/about">私たちについて</Link>
+              </li>
+              <li>
+                ― <Link to="/language-schools">スクール一覧</Link>
+              </li>
+              <li>
+                ― <Link to="/news">最新情報</Link>
+              </li>
+            </ul>
           </div>
           <div className="footer__col">
-            <h3>Contact Us</h3>
-            <Link to="/contact/form#contact">Contact Form</Link>
-            <Link to="/contact/telephone#contact">Phone</Link>
-            <Link to="mailto:contact@xlingual.co.jp">Email</Link>
-            <h3>Other</h3>
-            <Link to="/prices">Class Prices</Link>
-            <Link to="/privacy-policy">Privacy policy</Link>
+            <h3>お問い合わせ</h3>
+            <ul>
+              <li>
+                ―{" "}
+                <Link to="/contact/form#contact">フォームでのお問い合わせ</Link>
+              </li>
+              <li>
+                ―{" "}
+                <Link to="/contact/telephone#contact">
+                  電話でのお問い合わせe
+                </Link>
+              </li>
+              <li>
+                ―{" "}
+                <Link to="mailto:contact@xlingual.co.jp">
+                  Eメールでのお問い合わせ
+                </Link>
+              </li>
+            </ul>
+            <h3>その他</h3>
+            <ul>
+              <li>
+                ― <Link to="/privacy-policy">プライバシーポリシー</Link>
+              </li>
+            </ul>
           </div>
-        </div>
-        <div className="container footer__bottom">
-          <p>
-            © Copyright 2023 株式会社XLingual | All Rights Reserved | Website by
-            digital4.green
-          </p>
         </div>
       </footer>
     </>
