fa27c07d268ef916cbc3a86bb5ea113424b57938
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-01-30 15:26:27 +0100

    Add swoosh to blog lesson pages

--- app/routes/_front.blog-lessons.tsx
+++ app/routes/_front.blog-lessons.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "@remix-run/react";
 import { type LinksFunction } from "@remix-run/node";
 
 import lessonStyles from "../styles/lessons.css";
+import { Swoosh1 } from "~/components/swooshes";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: lessonStyles },
@@ -14,6 +15,7 @@ export default function LessonsParentPage() {
   return (
     <>
       <Outlet />
+      <Swoosh1 swooshColor="beige" backColor="cream" />
     </>
   );
 }
