464ceec823866f3de0db384503860a5254170be6
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-02-07 10:40:55 +0100

    Refactor footer code - stage one

--- app/routes/_front.tsx
+++ app/routes/_front.tsx
@@ -8,93 +8,100 @@ export default function FrontParentRoute() {
       <div className="content-wrapper">
         <Outlet />
       </div>
-      <footer className="footer">
-        <div className="container footer__inner">
-          <div className="footer__branding-col one">
-            <Link to="/">
-              <img
-                className="g-footer__logo"
-                src="/img/logo_with_tagline.svg"
-                alt="XLingual logo"
-              ></img>
+      <footer className="g-footer">
+        <div className="container g-footer__inner">
+          <div className="g-footer__branding-col one">
+            <Link className="g-footer__logo" to="/">
+              <img src="/img/logo_with_tagline.svg" alt="XLingual logo"></img>
             </Link>
-            <div className="footer__social">
-              <Link to="#">
-                <FaInstagram className="footer__social-icon" />
+            <div className="g-footer__socials">
+              <Link className="g-footer__social instagram" to="#">
+                <FaInstagram />
+                <span>Instagram | Language Learning</span>
               </Link>
-              <Link to="#">
-                <FaFacebookF className="footer__social-icon" />
+              <Link to="#" className="g-footer__social regular">
+                <FaFacebookF />
+                <span>Facebook</span>
               </Link>
-              <Link to="#">
-                <FaYoutube className="footer__social-icon" />
+              <Link className="g-footer__social instagram" to="#">
+                <FaInstagram />
+                <span>Instagram | News</span>
+              </Link>
+              <Link className="g-footer__social regular" to="#">
+                <FaYoutube />
+                <span>Youtube</span>
               </Link>
             </div>
-            <div className="footer__copyright">
+            <div className="g-footer__copyright">
               <p>
                 © Copyright 2023 株式会社XLingual | All Rights Reserved |
                 Website by digital4.green
               </p>
             </div>
           </div>
-          <div className="footer__col">
-            <h3>言語学習</h3>
-            <ul>
-              <li>
-                ― <Link to="/courses">コース紹介</Link>
-              </li>
-              <li>
-                ― <Link to="/prices">料金</Link>
-              </li>
-              <li>
-                ―{" "}
-                <Link to="/learning-experiences">
-                  ランゲージ・エクスペリエンス
-                </Link>
-              </li>
-              <li>
-                ― <Link to="/courses">読んで学べるブログ</Link>
-              </li>
-            </ul>
+          <div className="g-footer__links-col">
+            <div>
+              <h3>言語学習</h3>
+              <ul>
+                <li>
+                  ― <Link to="/courses">コース紹介</Link>
+                </li>
+                <li>
+                  ― <Link to="/prices">料金</Link>
+                </li>
+                <li>
+                  ―{" "}
+                  <Link to="/learning-experiences">
+                    ランゲージ・エクスペリエンス
+                  </Link>
+                </li>
+                <li>
+                  ― <Link to="/courses">読んで学べるブログ</Link>
+                </li>
+              </ul>
 
-            <h3>会社案内</h3>
-            <ul>
-              <li>
-                ― <Link to="/about">私たちについて</Link>
-              </li>
-              <li>
-                ― <Link to="/language-schools">スクール一覧</Link>
-              </li>
-              <li>
-                ― <Link to="/news">最新情報</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="footer__col">
-            <h3>お問い合わせ</h3>
-            <ul>
-              <li>
-                ―{" "}
-                <Link to="/contact/form#contact">フォームでのお問い合わせ</Link>
-              </li>
-              <li>
-                ―{" "}
-                <Link to="/contact/telephone#contact">
-                  電話でのお問い合わせe
-                </Link>
-              </li>
-              <li>
-                ―{" "}
-                <Link to="mailto:contact@xlingual.co.jp">
-                  Eメールでのお問い合わせ
-                </Link>
-              </li>
-            </ul>
-            <h3>その他</h3>
-            <ul>
-              <li>
-                ― <Link to="/privacy-policy">プライバシーポリシー</Link>
-              </li>
-            </ul>
+              <h3>会社案内</h3>
+              <ul>
+                <li>
+                  ― <Link to="/about">私たちについて</Link>
+                </li>
+                <li>
+                  ― <Link to="/language-schools">スクール一覧</Link>
+                </li>
+                <li>
+                  ― <Link to="/news">最新情報</Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3>お問い合わせ</h3>
+              <ul>
+                <li>
+                  ―{" "}
+                  <Link to="/contact/form#contact">
+                    フォームでのお問い合わせ
+                  </Link>
+                </li>
+                <li>
+                  ―{" "}
+                  <Link to="/contact/telephone#contact">
+                    電話でのお問い合わせe
+                  </Link>
+                </li>
+                <li>
+                  ―{" "}
+                  <Link to="mailto:contact@xlingual.co.jp">
+                    Eメールでのお問い合わせ
+                  </Link>
+                </li>
+              </ul>
+              <h3>その他</h3>
+              <ul>
+                <li>
+                  ― <Link to="/privacy-policy">プライバシーポリシー</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </footer>
