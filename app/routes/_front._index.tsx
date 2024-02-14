055e5418ec99445a994c3dc6d4df165e6d8f08b3
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-02-14 17:55:40 +0100

    Home services brown to green

--- app/routes/_front._index.tsx
+++ app/routes/_front._index.tsx
@@ -153,7 +153,7 @@ export default function Index() {
       </section>
 
       <section id="services">
-        <Swoosh1 backColor="cream" swooshColor="brown" />
+        <Swoosh1 backColor="cream" swooshColor="green" />
         <div className="ho-services">
           <div className="g-grid-container1">
             <div className="ho-services__heading">
