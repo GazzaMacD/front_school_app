aa8bdc8a89e774e66d390d367958311394a89657
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-02-16 18:13:50 +0100

    Add header and nav for xl screens to contact page

--- app/routes/_front.contact._index.tsx
+++ app/routes/_front.contact._index.tsx
@@ -3,7 +3,42 @@ import { json } from "@remix-run/node";
 
 import { BASE_API_URL } from "~/common/constants.server";
 import { getGlobalEnv } from "~/common/utils";
+import { SlidingHeaderPage } from "~/components/pages";
+import { FaArrowDown } from "react-icons/fa6";
+/**
+ *  Utils and helper functions
+ */
+const contactMenu = [
+  {
+    id: 1,
+    text: "無料体験レッスン",
+    url: "#trial",
+  },
+  {
+    id: 2,
+    text: "ランゲージ・エクスペリエンス",
+    url: "#experience",
+  },
+  {
+    id: 3,
+    text: "よくある質問",
+    url: "#qanda",
+  },
+  {
+    id: 4,
+    text: "お電話でのお問い合わせ",
+    url: "#telephone",
+  },
+  {
+    id: 5,
+    text: "お問い合わせフォーム",
+    url: "#form",
+  },
+];
 
+/**
+ * Server functions
+ */
 export async function loader() {
   try {
     const apiUrl = `${BASE_API_URL}/pages/?slug=contact&type=contacts.ContactPage&fields=*`;
@@ -23,8 +58,39 @@ export default function ContactIndexPage() {
   const ENV = getGlobalEnv();
   const { page } = useLoaderData<typeof loader>();
   return (
-    <div>
-      <div>Work here</div>
-    </div>
+    <SlidingHeaderPage
+      mainTitle={page.title}
+      subTitle={page.display_title}
+      swooshBackColor="cream"
+      swooshFrontColor="beige"
+    >
+      <div className="ct-nav">
+        {contactMenu.map((item) => {
+          return <PageNav key={item.id} text={item.text} url={item.url} />;
+        })}
+      </div>
+    </SlidingHeaderPage>
+  );
+}
+
+/*
+ * Components
+ */
+
+type TPageNavProps = {
+  text: string;
+  url: string;
+};
+
+function PageNav({ text, url }: TPageNavProps) {
+  return (
+    <Link className="ct-nav__link " to={url}>
+      <div className="ct-nav__btn">
+        <div className="ct-nav__icon">
+          <FaArrowDown />
+        </div>
+        <div className="ct-nav__text">{text}</div>
+      </div>
+    </Link>
   );
 }
--- app/routes/_front.contact.tsx
+++ app/routes/_front.contact.tsx
@@ -6,11 +6,15 @@ import { getGlobalEnv } from "~/common/utils";
 
 import contactStyles from "~/styles/contact.css";
 import { BASE_API_URL } from "~/common/constants.server";
+import pageCStyles from "~/styles/components/pages.css";
+import cardStyles from "~/styles/components/cards.css";
 
 /* Types */
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: contactStyles },
+  { rel: "stylesheet", href: pageCStyles },
+  { rel: "stylesheet", href: cardStyles },
 ];
 
 export default function ContactParentPage() {
