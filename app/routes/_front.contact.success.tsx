ee39235448f4b016d7f15056867ef1dba0b6c810
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-02-26 10:48:31 +0100

    Refactor contact form xl screen

--- app/routes/_front.contact.success.tsx
+++ app/routes/_front.contact.success.tsx
@@ -1,21 +1,25 @@
 import { Link } from "@remix-run/react";
+import * as React from "react";
 import { AiOutlineSmile } from "react-icons/ai";
 import { redirect, type LoaderArgs } from "@remix-run/node";
+import * as Dialog from "@radix-ui/react-dialog";
 
 /*
  * Server Functions
  */
 export function loader({ request }: LoaderArgs) {
   const referer = request.headers.get("referer");
+  /*
   if (!referer || new URL(referer).pathname !== "/contact") {
     return redirect("/");
   }
+  */
   return null;
 }
 
-export default function ContactTelephonePage() {
+export default function ContactSuccessPage() {
   return (
-    <div>
+    <div className="ct-success">
       <h3>
         <AiOutlineSmile /> Success, thank you!
       </h3>
