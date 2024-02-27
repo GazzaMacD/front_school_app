93df7c2e9e2bcfdd740738afa1192366e67deb10
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-02-27 11:22:36 +0100

    Refactor sucess page

--- app/routes/_front.contact.success.tsx
+++ app/routes/_front.contact.success.tsx
@@ -1,8 +1,10 @@
-import { Link } from "@remix-run/react";
 import * as React from "react";
 import { AiOutlineSmile } from "react-icons/ai";
+import { Link } from "@remix-run/react";
 import { redirect, type LoaderArgs } from "@remix-run/node";
-import * as Dialog from "@radix-ui/react-dialog";
+
+import { HeadingOne } from "~/components/headings";
+import { Swoosh1 } from "~/components/swooshes";
 
 /*
  * Server Functions
@@ -19,17 +21,32 @@ export function loader({ request }: LoaderArgs) {
 
 export default function ContactSuccessPage() {
   return (
-    <div className="ct-success">
-      <h3>
-        <AiOutlineSmile /> Success, thank you!
-      </h3>
-      <p>
-        Thanks so much for contacting us, we will be in touch with you soon.
-      </p>
-      <p>
-        If you have some time, why don't you read one of our{" "}
-        <Link to="/lessons">free lessons</Link>.
-      </p>
-    </div>
+    <>
+      <div className="ct-success">
+        <div className="g-narrow-container">
+          <div className="ct-success__heading">
+            <HeadingOne
+              enText="Success"
+              jpText="送信しました"
+              align="center"
+              bkground="light"
+              level="h1"
+            />
+          </div>
+          <div className="ct-success__details">
+            <p>
+              Thanks so much for contacting us, we will be in touch with you
+              soon.
+            </p>
+            <p>
+              If you have some time, why don't you read one of our{" "}
+              <Link to="/blog-lessons">free blog lessons</Link>. Alternatively,
+              please go to our <Link to="/">home page.</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Swoosh1 backColor="cream" swooshColor="beige" />
+    </>
   );
 }
