0bda94421b7c67e3662bba61605f8a161c049ccf
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-03-12 05:12:55 +0100

    School list page design for xl and mobile completed

--- app/routes/_front.language-schools._index.tsx
+++ app/routes/_front.language-schools._index.tsx
@@ -8,10 +8,16 @@ import {
 import { BASE_API_URL } from "~/common/constants.server";
 import { getGlobalEnv } from "~/common/utils";
 import { getTitle } from "~/common/utils";
+import { SlidingHeaderPage } from "~/components/pages";
+import pageStyles from "~/styles/components/pages.css";
 
 /**
  *  Utils and helper functions
  */
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: pageStyles },
+];
+
 export const meta: V2_MetaFunction = () => {
   return [
     { title: getTitle({ title: "Language Schools・語学学校", isHome: false }) },
@@ -24,7 +30,7 @@ export const meta: V2_MetaFunction = () => {
 export async function loader() {
   try {
     const lPageUrl = `${BASE_API_URL}/pages/?type=languageschools.LanguageSchoolListPage&fields=*`;
-    const dPageUrl = `${BASE_API_URL}/pages/?type=languageschools.LanguageSchoolDetailPage&fields=_,id,title,display_title,slug,display_city,header_image`;
+    const dPageUrl = `${BASE_API_URL}/pages/?type=languageschools.LanguageSchoolDetailPage&fields=_,id,title,display_title,display_tagline,slug,display_city,header_image`;
     const urls = [lPageUrl, dPageUrl];
     const [lPage, dPage] = await Promise.all(
       urls.map((url) =>
@@ -60,6 +66,55 @@ export default function LanguageSchoolsListPage() {
   const { listPage: lp, dPages: dp } = useLoaderData<typeof loader>();
 
   return (
+    <SlidingHeaderPage
+      mainTitle={lp.title}
+      subTitle={lp.display_title}
+      swooshBackColor="cream"
+      swooshFrontColor="beige"
+    >
+      <div className="ls-lp-intro">
+        <div
+          className="g-narrow-container"
+          dangerouslySetInnerHTML={{ __html: lp.display_intro }}
+        />
+      </div>
+      <section className="ls-lp-schools">
+        <div className="g-grid-container1">
+          {dp.map((school, i) => {
+            return (
+              <article key={school.id} className={`ls-lp-school t${i % 2}`}>
+                <div className="ls-lp-school__img-wrap">
+                  <img
+                    src={`${ENV.BASE_BACK_URL}${school.header_image.thumbnail.src}`}
+                    alt={school.display_title}
+                  />
+                </div>
+                <div className="ls-lp-school__details">
+                  <div className="ls-lp-school__heading">
+                    <h3>{school.display_title}校</h3>
+                    <p>愛知県{school.display_city}</p>
+                  </div>
+                  <p className="ls-lp-school__tagline">
+                    {school.display_tagline}
+                  </p>
+                  <Link
+                    className="ls-lp-school__link"
+                    to={`/language-schools/${school.meta.slug}`}
+                    key={school.id}
+                  >
+                    詳しく見る
+                  </Link>
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    </SlidingHeaderPage>
+  );
+}
+
+/*
     <div>
       <h1>{lp.title}</h1>
       <p>{lp.display_title}</p>
@@ -86,5 +141,4 @@ export default function LanguageSchoolsListPage() {
         })}
       </div>
     </div>
-  );
-}
+    */
