0a3cb31fea1e9d4c6dcb2be1dc11e61f6724dc81
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-03-18 10:01:04 +0100

    Email confirmed page completed with new design

--- app/routes/_front._auth.email-confirmed.tsx
+++ app/routes/_front._auth.email-confirmed.tsx
@@ -1,19 +1,49 @@
-import { type LoaderArgs, json } from "@remix-run/node";
+import { type V2_MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 
-export const loader = async ({ request }: LoaderArgs) => {
-  console.log(request.headers);
-  return json({});
+import { getTitle } from "~/common/utils";
+import { SlidingHeaderPage } from "~/components/pages";
+
+/*
+ * Helper functions
+ */
+export const meta: V2_MetaFunction = () => {
+  return [
+    {
+      title: getTitle({
+        title: "Email Confirmed・メールを確認しました",
+        isHome: false,
+      }),
+    },
+  ];
 };
 
-export default function EmailConfirmedRoute() {
+/*
+ * Client Functions
+ */
+export default function EmailConfirmedSuccessPage() {
   return (
     <>
-      <h1 className="auth__heading">Email Confirmed</h1>
-      <p>
-        Thanks for confirming your email. Please <Link to="/login">login</Link>{" "}
-        or start browsing our site from the <Link to="/">home page</Link>.
-      </p>
+      <SlidingHeaderPage
+        mainTitle="Email Confirmed"
+        subTitle="メールを確認しました"
+        swooshBackColor="cream"
+        swooshFrontColor="beige"
+      >
+        <div className="au-wrapper">
+          <div className="au-wrapper__inner">
+            <div className="au-form">
+              <div className="au-form__message">
+                <p>
+                  Thanks for confirming your email. Please{" "}
+                  <Link to="/login">login</Link> or start browsing our site from
+                  the <Link to="/">home page</Link>.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SlidingHeaderPage>
     </>
   );
 }
