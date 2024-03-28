a7b7c3c7f6c71ed38f6953f791eceae88366eb79
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-03-28 06:35:51 +0100

    Price plan detail page about section implemented for all screen sizes

--- app/routes/_front.price-plans.$slug.tsx
+++ app/routes/_front.price-plans.$slug.tsx
@@ -7,6 +7,8 @@ import { BASE_API_URL } from "~/common/constants.server";
 import { getTitle } from "~/common/utils";
 import { ClassPricePlanTable } from "~/components/prices";
 import { getGlobalEnv } from "~/common/utils";
+import { HeadingOne } from "~/components/headings";
+import { Swoosh1 } from "~/components/swooshes";
 
 /**
  * Helper functions
@@ -28,6 +30,7 @@ export const meta: V2_MetaFunction = () => {
 export async function loader({ params }: LoaderArgs) {
   const { slug } = params;
   const url = `${BASE_API_URL}/pages/?slug=${slug}&type=products.ClassPricesDetailPage&fields=*`;
+  console.log(url);
   const res = await fetch(url);
   const data = await res.json();
   if (!res.ok || !data.items.length) {
@@ -38,7 +41,9 @@ export async function loader({ params }: LoaderArgs) {
 }
 
 export default function PricePlansDetailPage() {
-  const { page: p } = useLoaderData<typeof loader>();
+  const { page } = useLoaderData<typeof loader>();
+  const p = page.class_service;
+  const pi = page.class_service.price_info;
   const ENV = getGlobalEnv();
   return (
     <>
@@ -46,19 +51,103 @@ export default function PricePlansDetailPage() {
         <div className="g-basic-container">
           <div className="pp-dp-header__titles">
             <h1>
-              {p.display_title}
-              <span>{p.title}</span>
+              {page.display_title}
+              <span>{page.title}</span>
             </h1>
-            <p>{p.display_tagline}</p>
+            <p>{page.display_tagline}</p>
           </div>
         </div>
         <div className="pp-dp-header__img-wrap">
           <img
-            src={`${ENV.BASE_BACK_URL}${p.header_image.medium.src}`}
-            alt={p.header_image.medium.alt}
+            src={`${ENV.BASE_BACK_URL}${page.header_image.medium.src}`}
+            alt={page.header_image.medium.alt}
           />
         </div>
       </header>
+
+      <section id="about">
+        <div className="g-narrow-container">
+          <HeadingOne
+            enText="About Plan"
+            jpText="プランについて"
+            align="center"
+            bkground="light"
+            level="h2"
+          />
+        </div>
+        <div className="pp-dp-about__intro">
+          {page.class_intro.map((block: any) => {
+            if (block.type === "rich_text") {
+              return (
+                <div
+                  className="g-narrow-container"
+                  key={block.id}
+                  dangerouslySetInnerHTML={{ __html: block.value }}
+                />
+              );
+            } else if (block.type === "text_width_img") {
+              return (
+                <div key={block.id} className="g-narrow-container">
+                  <figure className="pp-dp-about__img-wrapper text-width">
+                    <img
+                      src={`${ENV.BASE_BACK_URL}${block.value.image.original.src}`}
+                      alt={block.value.image.original?.alt}
+                    />
+                    {block.value?.caption ? (
+                      <figcaption>{block.value.caption}</figcaption>
+                    ) : null}
+                  </figure>
+                </div>
+              );
+            } else if (block.type === "youtube") {
+              return (
+                <div key={block.id}>
+                  <div className="pp-dp-intro__youtube">
+                    <iframe
+                      className={`g-youtube-iframe ${
+                        block.value.short ? "g-youtube-short" : ""
+                      }`}
+                      src={`${block.value.src}?modestbranding=1&controls=0&rel=0`}
+                      title="YouTube video player"
+                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+        <div className="pp-dp-about__price">
+          <ClassPricePlanTable
+            color="beige"
+            showLinkButton={false}
+            slug={p.slug}
+            titleEn={page.title}
+            titleJa={page.display_title}
+            duration={p.length}
+            durationUnit={p.length_unit}
+            stdQuantity={p.quantity}
+            stdQuantityUnit={p.quantity_unit}
+            maxNum={p.max_num}
+            isNative={p.is_native}
+            isOnline={p.is_online}
+            isInperson={p.is_inperson}
+            hasOnlineNotes={p.has_onlinenotes}
+            bookableOnline={p.bookable_online}
+            preTaxPrice={pi.pretax_price}
+            postTaxPrice={pi.posttax_price}
+            onSale={pi.is_sale}
+            preSalePreTaxPrice={pi.before_sale_pretax_price}
+            preSalePostTaxPrice={pi.before_sale_posttax_price}
+            priceStartDate={pi.start_date}
+            priceEndDate={pi.end_date}
+          />
+        </div>
+      </section>
+      <Swoosh1 swooshColor="beige" backColor="cream" />
     </>
   );
 }
