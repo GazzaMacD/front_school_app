e12dda471e4af092fbeafceadd5a80232474ee72
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-04-19 10:57:50 +0900

    Update error handling for price plans

--- app/routes/_front.price-plans.tsx
+++ app/routes/_front.price-plans.tsx
@@ -1,10 +1,17 @@
-import { Outlet } from "@remix-run/react";
+import {
+  Link,
+  Outlet,
+  isRouteErrorResponse,
+  useRouteError,
+} from "@remix-run/react";
 import { type LinksFunction } from "@remix-run/node";
 
 import pricePlansStyles from "~/styles/price-plans.css";
 import pricesStyles from "~/styles/components/prices.css";
 import swipperStyles from "swiper/css";
 import swipperNavStyles from "swiper/css/navigation";
+import { SwooshErrorPage } from "~/components/errors";
+import { MESSAGES } from "~/common/languageDictionary";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: swipperStyles },
@@ -22,4 +29,49 @@ export default function PricePlansParentPage() {
       <Outlet />
     </>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404) {
+      return (
+        <SwooshErrorPage
+          title={MESSAGES.ja.statusCode.HTTP404}
+          text={MESSAGES.ja.errorBoundaryText.clickLink}
+          linkUrl="/"
+          linkText={MESSAGES.ja.errorBoundaryLinkTexts.home}
+        />
+      );
+    } else if (error.status !== 500) {
+      return (
+        <SwooshErrorPage
+          title={MESSAGES.ja.statusCode.HTTP500}
+          text={MESSAGES.ja.errorBoundaryText.clickLink}
+          linkUrl="/"
+          linkText={MESSAGES.ja.errorBoundaryLinkTexts.home}
+        />
+      );
+    } else {
+      return (
+        <SwooshErrorPage
+          title={MESSAGES.ja.statusCode.HTTP500}
+          text={MESSAGES.ja.errorBoundaryText.tryLater}
+        />
+      );
+    }
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+        <Link to="/">Go home</Link>
+      </div>
+    );
+  } else {
+    return <h1>500 - Sorry looks like something went wrong on the server</h1>;
+  }
 }
