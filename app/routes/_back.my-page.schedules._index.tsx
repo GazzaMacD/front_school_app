fab905f62b4c655ecbab379303d23253c680b3f5
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-06-18 09:25:40 +0200

    Improve ux for schedules list page, minor update to remix.config.js to fix dev server issue

--- app/routes/_back.my-page.schedules._index.tsx
+++ app/routes/_back.my-page.schedules._index.tsx
@@ -1,4 +1,5 @@
 import { useMatches, Link, useLoaderData, Outlet } from "@remix-run/react";
+import { BsCalendarWeek } from "react-icons/bs";
 
 export default function SchedulesIndexRoute() {
   const matches = useMatches();
@@ -8,12 +9,23 @@ export default function SchedulesIndexRoute() {
 
   return (
     <div>
+      <p>
+        Please note that schedules for physical schools allow both online and
+        face to face lessons if the time is open.
+      </p>
       {Object.entries(schedulesBySchool).map(([schoolName, school]) => (
-        <div key={schoolName}>
+        <div key={schoolName} className="sc-lp-school__schedule">
           <h2>{schoolName}</h2>
           {school.map((scheduleObj) => (
-            <Link key={scheduleObj.id} to={`${scheduleObj.slug}`}>
-              <div>{scheduleObj.teacherName}'s Booking Schedule</div>
+            <Link
+              key={scheduleObj.id}
+              className="sc-lp-school__schedule__link"
+              to={`${scheduleObj.slug}`}
+            >
+              <div>
+                <BsCalendarWeek />
+                <span>{scheduleObj.teacherName}'s Booking Schedule</span>
+              </div>
             </Link>
           ))}
         </div>
