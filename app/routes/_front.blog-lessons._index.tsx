a314d586c8099dfa2fece74632fd5d98fda1de5c
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-09-16 16:02:01 +0200

    Add load more button functionality to blog lessons list page

--- app/routes/_front.blog-lessons._index.tsx
+++ app/routes/_front.blog-lessons._index.tsx
@@ -1,37 +1,60 @@
 import React from "react";
-import { json, type MetaFunction, type LinksFunction } from "@remix-run/node";
-import { useSearchParams } from "@remix-run/react";
-import { Link, useFetcher, useLoaderData } from "@remix-run/react";
+import {
+  json,
+  type MetaFunction,
+  type LinksFunction,
+  type LoaderFunctionArgs,
+} from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
 import { FaArrowRightLong } from "react-icons/fa6";
 
-import { getTitle } from "~/common/utils";
 import { BASE_API_URL } from "~/common/constants.server";
-import { handlePreview } from "~/common/utils.server";
-import { getGlobalEnv } from "~/common/utils";
-import { HeadingOne } from "~/components/headings";
-import { Swoosh1 } from "~/components/swooshes";
+import { getGlobalEnv, getTitle } from "~/common/utils";
 import pageStyles from "~/styles/components/pages.css";
-
-/*types */
-import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
-import type {
-  TBaseDetailPage,
-  TBaseListPage,
-  TListPageItemAllMeta,
-} from "~/common/types";
+import { BLOG_LESSONS_LIMIT, BLOG_LESSONS_OFFSET } from "~/common/constants";
 import { SlidingHeaderPage } from "~/components/pages";
 
-type TLessonsPreview = TBaseDetailPage & {
-  jp_title: string;
+type TBlogLesson = {
+  id: number;
+  meta: {
+    slug: string;
+  };
+  title: string;
+  header_image: {
+    id: number;
+    title: string;
+    original: {
+      src: string;
+      width: number;
+      height: number;
+      alt: string;
+    };
+    medium: {
+      src: string;
+      width: number;
+      height: number;
+      alt: string;
+    };
+    thumbnail: {
+      src: string;
+      width: number;
+      height: number;
+      alt: string;
+    };
+    alt: string;
+  };
+  display_title: string;
+  display_tagline: string;
+  published_date: string;
+  category: {
+    id: number;
+    name: string;
+    ja_name: string;
+    slug: string;
+  };
 };
 
-type TLesson = TListPageItemAllMeta & {
-  jp_title: string;
-};
-
-type TLessons = TBaseListPage & {
-  items: TLesson[];
-};
+type TBlogLessons = TBlogLesson[];
 
 /**
  * Helper functions
@@ -51,63 +74,109 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+function deDuplicateLessons(lessons: TBlogLessons): TBlogLessons {
+  const lessonTracker: { [key: string]: "exists" } = {};
+  const deDuplicated = lessons.filter((lesson) => {
+    if (lessonTracker.hasOwnProperty(String(lesson.id))) {
+      return false;
+    }
+    lessonTracker[String(lesson.id)] = "exists";
+    return true;
+  });
+  return deDuplicated;
+}
+
 /*
  * Loader and Action functions
  */
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const category = new URL(request.url).searchParams.get("category");
-  const pageUrl = `${BASE_API_URL}/pages/?type=lessons.LessonListPage&fields=_,title,display_title`;
-  const categoriesUrl = `${BASE_API_URL}/lesson-categories/`;
-  const urls = [pageUrl, categoriesUrl];
-  try {
-    const [page, categories] = await Promise.all(
-      urls.map((url) =>
-        fetch(url)
-          .then(async (r) => {
-            return {
-              data: await r.json(),
-              status: r.status,
-              ok: r.ok,
-            };
-          })
-          .then((data) => {
-            return {
-              data: data.data,
-              status: data.status,
-              ok: data.ok,
-              url: url,
-            };
-          })
-          .catch((error) => ({ error, url }))
-      )
-    );
-    if (!page.ok || !categories.ok || !page.data.items.length) {
-      throw new Response("Oops that's 404", { status: 404 });
-    }
-    // can now get detail page
-    let detailUrl = `${BASE_API_URL}/pages/?order=-published_date&type=lessons.LessonDetailPage&fields=_,id,slug,display_title,display_tagline,published_date,title,header_image,category`;
-    if (category) {
-      const filtered = categories.data.filter((c) => c.ja_name === category);
-      const target = filtered.length ? filtered[0] : null;
-      detailUrl = target ? detailUrl + `&category=${target.id}` : detailUrl;
-    }
-    const res = await fetch(detailUrl);
-    const data = await res.json();
-    if (!res.ok) {
-      throw new Response("Oops that's 404", { status: 404 });
-    }
+  const page = new URL(request.url).searchParams.get("page");
 
-    return json({
-      page: page.data.items[0],
-      categories: categories.data,
-      lessonsData: data,
-    });
-  } catch (error) {
-    throw new Response("Oops sorry something went wrong", {
-      status: 500,
-    });
+  // route for fetcher load more
+
+  // route for normal page load
+  const indexPageUrl = `${BASE_API_URL}/pages/?type=lessons.LessonListPage&fields=_,title,display_title`;
+  const categoriesUrl = `${BASE_API_URL}/lesson-categories/`;
+  const urls = [indexPageUrl, categoriesUrl];
+  const [indexPage, categoriesData] = await Promise.all(
+    urls.map((url) =>
+      fetch(url)
+        .then(async (r) => {
+          return {
+            data: await r.json(),
+            status: r.status,
+            ok: r.ok,
+          };
+        })
+        .then((data) => {
+          return {
+            data: data.data,
+            status: data.status,
+            ok: data.ok,
+            url: url,
+            error: null,
+          };
+        })
+        .catch((error) => ({
+          data: null,
+          status: 500,
+          ok: false,
+          url,
+          error,
+        }))
+    )
+  );
+  if (!indexPage.ok || !categoriesData.ok || !indexPage.data.items.length) {
+    throw new Response("Oops that's 404", { status: 404 });
   }
+  /* ===== get detail pages ===== */
+  let detailUrl = `${BASE_API_URL}/pages/?order=-published_date&type=lessons.LessonDetailPage&fields=_,id,slug,display_title,display_tagline,published_date,title,header_image,category&limit=${BLOG_LESSONS_LIMIT}`;
+
+  //categories
+  const categories = [
+    ...[{ id: 0, name: "All", ja_name: "全レッスン" }],
+    ...categoriesData.data,
+  ];
+  const filtered = categories.filter((c) => c.ja_name === category);
+  const selectedCategory = filtered.length ? filtered[0] : categories[0];
+  detailUrl = filtered.length
+    ? detailUrl + `&category=${selectedCategory.id}`
+    : detailUrl;
+
+  /* === pages === */
+  // determine if page number is coming from load more on list page
+  // If not then reset page number to 1
+  const referer = request.headers.get("referer");
+  const isFromListPage = Boolean(
+    referer && new URL(referer).pathname.includes("blog-lessons")
+  );
+  const pageNumber =
+    isFromListPage &&
+    page &&
+    !Number.isNaN(Number.parseInt(page)) &&
+    Number.parseInt(page) > 1
+      ? Number.parseInt(page)
+      : 1;
+  detailUrl =
+    pageNumber > 1
+      ? detailUrl + `&offset=${(pageNumber - 1) * BLOG_LESSONS_OFFSET}`
+      : detailUrl;
+  const res = await fetch(detailUrl);
+  const lessonsData = await res.json();
+  if (!res.ok) {
+    throw new Response("Oops that's 404", { status: 404 });
+  }
+
+  return json({
+    page: indexPage.data.items[0],
+    categories,
+    selectedCategory,
+    lessonsData,
+    pageNumber,
+    limit: BLOG_LESSONS_LIMIT,
+  });
 }
 
 /*
@@ -116,11 +185,44 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function BlogLessonsIndexPage() {
   const ENV = getGlobalEnv();
-  const [searchParams] = useSearchParams();
-  const selectedCat = searchParams.get("category");
-  const { page, categories: c, lessonsData } = useLoaderData<typeof loader>();
-  const categories = [...[{ id: 0, name: "All", ja_name: "全レッスン" }], ...c];
-  const lessons = lessonsData.items;
+  //loader data
+  const { page, categories, selectedCategory, lessonsData, pageNumber, limit } =
+    useLoaderData<typeof loader>();
+  // categories
+  //lessons
+  const initLessons = lessonsData.items;
+  const [lessons, setLessons] = React.useState<TBlogLessons>(initLessons);
+  const [currentCategory, setCurrentCategory] =
+    React.useState(selectedCategory);
+  const [loadMore, setLoadMore] = React.useState(false);
+  const totalLessonCount = lessonsData.meta.total_count;
+  const currentLessonCount = lessons.length;
+  const totalPages = Math.ceil(totalLessonCount / limit);
+
+  /* === Effects === */
+  React.useEffect(() => {
+    const previousPage = Number(sessionStorage.getItem("previous-page"));
+    if (!previousPage) {
+      sessionStorage.setItem("previous-page", String(pageNumber));
+      return;
+    }
+    if (previousPage === pageNumber) return;
+    setLessons((old) => deDuplicateLessons([...old, ...lessonsData.items]));
+    sessionStorage.setItem("previous-page", String(pageNumber));
+    setLoadMore(false);
+  }, [loadMore, pageNumber, lessonsData.items]);
+
+  React.useEffect(() => {
+    if (currentCategory.id !== selectedCategory.id) {
+      setCurrentCategory(() => selectedCategory);
+      setLessons(() => lessonsData.items);
+    }
+  }, [
+    selectedCategory.id,
+    currentCategory.id,
+    selectedCategory,
+    lessonsData.items,
+  ]);
 
   return (
     <SlidingHeaderPage
@@ -139,7 +241,7 @@ export default function BlogLessonsIndexPage() {
                   <Link
                     to="/blog-lessons"
                     className={`bl-lp-cat ${
-                      selectedCat == null ? "bl-lp-cat--active" : ""
+                      currentCategory.id === 0 ? "bl-lp-cat--active" : ""
                     }`}
                     key={category.id}
                   >
@@ -152,7 +254,7 @@ export default function BlogLessonsIndexPage() {
                     to={`/blog-lessons?category=${category.ja_name}`}
                     key={category.id}
                     className={`bl-lp-cat ${
-                      selectedCat === category.ja_name
+                      currentCategory.id === category.id
                         ? "bl-lp-cat--active"
                         : ""
                     }`}
@@ -204,6 +306,25 @@ export default function BlogLessonsIndexPage() {
                 })
               : null}
           </div>
+          {pageNumber < totalPages ? (
+            <>
+              <p className="bl-lp-post__see-more-count">
+                {totalLessonCount}投稿中{currentLessonCount}投稿を表示
+              </p>
+              <Link
+                to={`/blog-lessons?page=${pageNumber + 1}${
+                  currentCategory.id === 0
+                    ? ""
+                    : "&category=" + currentCategory.ja_name
+                }`}
+                onClick={() => setLoadMore(true)}
+                preventScrollReset
+                className="bl-lp-post__see-more-btn"
+              >
+                さらに読み込む
+              </Link>
+            </>
+          ) : null}
         </div>
       </section>
     </SlidingHeaderPage>
