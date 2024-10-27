ddce43bc69bf261d8ce75aaf5ee6b851dd97f794
Gareth Macdonald <macdonald.gareth@gmail.com>
2024-10-27 06:39:20 +0100

    Change root css stylesheet imports to include ?url

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,32 @@
 import {
-  Form,
+  type LinksFunction,
+  type LoaderFunctionArgs,
+  json,
+} from "@remix-run/node";
+import {
   Links,
   Meta,
   Scripts,
   ScrollRestoration,
+  Outlet,
 } from "@remix-run/react";
 
+import globalStyles from "~/styles/global.css?url";
+import fontStyles from "~/styles/fonts.css?url";
+import errorStyles from "~/styles/errors.css?url";
+
+/**
+ * helpers, loaders, actions
+ */
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: fontStyles },
+  { rel: "stylesheet", href: globalStyles },
+  { rel: "stylesheet", href: errorStyles },
+];
+
+/**
+ * Page
+ */
 export default function App() {
   return (
     <html lang="ja">
@@ -17,7 +38,7 @@ export default function App() {
       </head>
       <body>
         <h1>XLingual</h1>
-        <p>Update XLingual app to remix 2.13</p>
+        <p>Update XLingual app to remix 2.13.1</p>
         <ScrollRestoration />
         <Scripts />
       </body>
